216744f6119b5ecfcb55fa79564d5c857c30bf4e
renzynx <10262118-renzynx@users.noreply.gitlab.com>
2022-02-07 13:59:35 +0700

    update frontend invite link

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
       {
         source: "/invite",
         destination:
-          "https://discord.com/api/oauth2/authorize?client_id=772690931539247104&permissions=8&scope=bot%20applications.commands",
+          "https://discord.com/api/oauth2/authorize?client_id=772690931539247104&permissions=137475967040&scope=bot",
         permanent: true,
       },
     ];
