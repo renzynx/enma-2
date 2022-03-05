c5f161ee8bfe55a6f7336dee8eefb75f696e8ba5
renzynx <10262118-renzynx@users.noreply.gitlab.com>
2022-03-05 12:43:18 +0700

    remove useless stuff

--- frontend/src/pages/404.tsx
+++
@@ -1,7 +0,0 @@
-export const NotFound = () => {
-  return (
-    <div className="w-screen min-h-screen bg-slate-900 flex items-center justify-center">
-      <div className="text-[70pt] text-teal-400 text-center">404 Not Found</div>
-    </div>
-  );
-};
--- nextjs/lib/graphql/index.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-
-export const graphqlClient = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
-    credentials: "include",
-  }),
-});
