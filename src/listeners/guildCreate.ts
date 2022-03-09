8deab87a0dc38f2b67016dcc87706c1121e48e28
renzynx <10262118-renzynx@users.noreply.gitlab.com>
2022-03-09 14:28:11 +0700

    remove deprecated stuff

--- src/listeners/guildCreate.ts
+++ src/listeners/guildCreate.ts
@@ -8,15 +8,10 @@ import { GuildConfig } from '../entities/guild_config';
 export class UserEvent extends Listener {
 	private readonly guildRepository = getRepository(GuildConfig);
 	public async run(guild: Guild) {
-		if (this.container.client.id === '772690931539247104')
-			this.container.boat.postStats(this.container.client.guilds.cache.size, '772690931539247104');
 		const config = await this.guildRepository.findOne({ guild_id: guild.id });
-
 		if (config) {
-			console.log('Found');
 			return this.container.config.set(guild.id, config);
 		} else {
-			console.log('No Guild Found, Creating A New Record...');
 			const newConfig = this.guildRepository.create({ guild_id: guild.id });
 			const savedConfig = await this.guildRepository.save(newConfig);
 			return this.container.config.set(guild.id, savedConfig);
